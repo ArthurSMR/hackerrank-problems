38cb5a0dc9205c2a306da5000f44386c9d1c4b4e
ArthurSMR <arthursmr17@gmail.com>
2023-02-15 09:24:14 -0300

    Two Sum + Uber Test

--- HackerRank.playground/Pages/Breaking the Records.xcplaygroundpage/Contents.swift
+++ HackerRank.playground/Pages/Breaking the Records.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// https://www.hackerrank.com/challenges/three-month-preparation-kit-breaking-best-and-worst-records/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+
 
 func breakingRecords(scores: [Int]) -> [Int] {
 
---
+++ HackerRank.playground/Pages/Uber - Tip Coding interview.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+/*
+ The is a car with a certain number of seats that only drives forward in a single road 1000km long.
+ 
+ You are given the capacity of the car and an array of trips in which each trip has:
+ - The number of passengers
+ - The pick-up location
+ - The drop-off location
+ 
+ Example: given the trip [2,1,5], it means that 2 passengers will be picked up at 1st km and dropped off at 5th.
+ 
+ Write a function that answers whether this car is able to comply with all the given trips.
+ 
+
+ OUTPUTS EXAMPLES:
+ 
+ [[2, 3, 6], [1,2,3]], 4 -> true
+ [[2, 3, 6], [1,2,3]], 2 -> true
+ [[2, 3, 6]], 2 -> false
+ [[2, 3, 6]], 0 -> false
+ 
+ */
+
+func canFinishTrips(_ trips: [[Int]], capacity: Int) -> Bool {
+    
+    return false
+}
