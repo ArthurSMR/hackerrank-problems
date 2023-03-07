761c5ab3f071182e5050463839465d376fead7c0
ArthurSMR <arthursmr17@gmail.com>
2023-03-07 08:04:39 -0300

    Max Profit

--- HackerRank.playground/Pages/Best Time to Buy and Sell Stock.xcplaygroundpage/Contents.swift
+++ HackerRank.playground/Pages/Best Time to Buy and Sell Stock.xcplaygroundpage/Contents.swift
@@ -8,13 +8,29 @@ func maxProfit(_ prices: [Int]) -> Int {
     
     guard !prices.isEmpty else { return 0 }
     
-    var minPrice = prices[0]
-    var profit = 0
+    var lowestValue: Int = Int.max
+    var lowestDay: Int = 0
     
-    for price in prices {
+    var highestValue: Int = 0
+    
+    for day in 0..<prices.count {
+        
+        if prices[day] < lowestValue {
+            lowestValue = prices[day]
+            lowestDay = day
+        }
+        
+        if prices[day] > highestValue && day > lowestDay {
+            highestValue = prices[day]
+        }
     }
     
-    return 0
+    guard highestValue > lowestValue else { return 0 }
+    
+    var maximumProfit = highestValue - lowestValue
+    
+    return maximumProfit
 }
 
-maxProfit([7,1,5,3,6,4])
+maxProfit([7,1,5,3,6,4]) // 5
+maxProfit([7,6,4,3,1]) // 0
